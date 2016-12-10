b4b7d0d7e30656a8bdec2033724dc6a40817e326
Alfie <Alfie>
2016-12-10 22:32:44 +0800

    Initial Commit

---
+++ app/src/androidTest/java/jeadev/orolasa/ApplicationTest.java
@@ -0,0 +1,13 @@
+package jeadev.orolasa;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
